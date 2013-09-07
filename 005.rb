a50d7240061211e0e0084f60642b9c9a8b562d0b
Phill Baker <phill@generalassemb.ly>
2013-09-07 17:03:38 -0400

    Add solution to #5: 232792560.

--- 005.rb
+++ 005.rb
@@ -5,31 +5,16 @@
 # I.e. Find LCM of 1...20.
 # Naive multiple of 1...20 is 1*2*...*20.
 
-# LCM will also be a factor of prime numbers in (1,20) like 3, 5, 7, 11, 13,
-# 17, 19...anything that's divisible by 4 and 5 is also divisible by 20.
-# Approach: multiply largest primes, then other common factors as necessary.
-
-others = [2,4,6,8,9,10,12,14,15,16,18,20]
-primes = [3,5,7,11,13,17,19] 4,
-# product = primes.reduce(1, :*)
-
-# 232792560
-
-def divisible?(int)
-  20.step(2, -1) do |i|
-    if int % i != 0
-      puts "not divisible by #{i}"
-      return false 
-    end
-  end
-  true
+def gcd(a, b)
+  b == 0 ? a : gcd(b, a % b)
 end
 
-# other = others.last
-# while(!divisible?(product) || others.empty?)
-#   product *= other
-#   others.pop
-# end
+def lcm(a, b)
+ a * b / gcd(a, b)
+end
 
-# p others
-# puts product
+lcm = (1..20).inject(1) do |i,product|
+  lcm(i,product)
+end
+
+puts lcm
